8ac758724215a797387e4625623db697588a8c40
darshansatapara <darshansatapara286@gmail.com>
2026-01-13 01:22:48 +0530

    fix: update allowedOrigins to use a single production URL

--- backendshahcreation/server.js
+++ backendshahcreation/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [process.env.CLIENT_URL, process.env.LIVE_URL];
+const allowedOrigins = "https://shahcreation-cnja.vercel.app/";
 
 app.use(
   cors({
